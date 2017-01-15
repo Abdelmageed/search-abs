06832dd420d4cbc7d9e7d7a02175fb6157033707
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-01-15 19:58:59 +0200

    added a database connection

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require ('express'),      
-      app = express ()
+      app = express (),
+      mongo = require ('mongodb').MongoClient
 
-//app.use (express.static('/public'))
+mongo.connect (process.env._IMAGE_SEARCH_DB, (err, db) => {
+    if (err) throw err
+    console.log ('connected to database successfuly')
+    app.locals.db = db
+})
+app.use (express.static('public'))
+
 app.listen (process.env.PORT || 3000, function (){
     console.log (`express server listening to port ${this.address().port} in ${app.settings.env} mode`)
 })
