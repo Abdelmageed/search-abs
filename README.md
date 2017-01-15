d79d9298feed14fd954402ccba9311f917391ecc
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-01-15 20:47:55 +0200

    added a GET  /api/latest route to get the last 10 images. Added instructions on index.html. Added link to FCC challenge in README.md.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # search-abs
-FCC - API Challenges (4)
+[FCC - API Challenges (4)](https://www.freecodecamp.com/challenges/image-search-abstraction-layer)
--- routes.js
+++ routes.js
@@ -1,25 +1,60 @@
-const app = require ('./app'),
-      request = require ('request'),
-      baseApiUrl = 'https://www.googleapis.com/customsearch/v1'
+const app = require('./app'),
+    request = require('request'),
+    baseApiUrl = 'https://www.googleapis.com/customsearch/v1'
 
 
-app.get ('/api/:search', (req, res) => {
+app.get('/api/search/:search', (req, res) => {
     let s = `${baseApiUrl}?key=${process.env._GOOGLE_API_KEY}&cx=${process.env._CSE_ID}&q=${req.params.search}&start=${req.query.offset || 1}&alt=json&searchType=image&fields=items(link,snippet,image/thumbnailLink,image/contextLink)`
-    request (s, (err, response, body) => {
+    request(s, (err, response, body) => {
         if (err) throw err
         if (response.statusCode === 200) {
-            res.send(formatResult (body))
+            insertQuery(req.params.search, new Date())
+            res.send(formatResult(body))
         } else {
-            res.status(response.statusCode).send ('An error has occured')
+            res.status(response.statusCode).send('An error has occured')
         }
     })
 })
 
+app.get('/api/latest', (req, res) => {
+    getLatestQueries ((results) => {
+        console.log (results)
+        res.send (results)
+    })
+})
+
 const formatResult = (result) => {
-    return JSON.parse(result)['items'].map (item => ({
-                link: item.link,
-                snippet: item.snippet,
-                thumbnail: item.image.thumbnailLink,
-                context: item.image.contextLink
-            }))
+    return JSON.parse(result)['items'].map(item => ({
+        link: item.link,
+        snippet: item.snippet,
+        thumbnail: item.image.thumbnailLink,
+        context: item.image.contextLink
+    }))
+}
+
+const insertQuery = (query, at) => {
+    const db = app.locals.db
+    db.collection('queries').insertOne({
+        query: query,
+        at: at
+    }, (err, res) => {
+        if (err) throw err
+    })
+}
+
+const getLatestQueries = (callback) => {
+    const db = app.locals.db
+    db.collection('queries')
+        .find({})
+        .sort([['_id', -1]])
+        .limit(10)
+        .project({
+            _id: 0,
+            query: 1,
+            at: 1
+        })
+        .toArray((err, res) => {
+            if (err) throw err
+            callback(res)
+        })
 }
